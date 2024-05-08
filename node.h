6c96afad4fefca5db56b0d4484ad5e2778510f25
황상현 <sanghyeon@hwangsanghyeon-ui-MacBookPro.local>
2024-05-08 14:14:54 +0900

    init files

---
+++ node.h
@@ -0,0 +1,18 @@
+#ifndef NODE_H
+#define NODE_H
+
+class Node {
+  // TODO: Node 클래스 정의 수정할 것. 함수, 변수 추가 불가능
+
+public:
+  Node(int value) {
+    value_ = value;
+    next_ = nullptr;
+  }
+
+private:
+  int value_;
+  Node *next_;
+};
+
+#endif
